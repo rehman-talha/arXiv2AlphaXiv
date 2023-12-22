bc1f238f7ffc9abae41605e395fb31a920a3ff84
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2023-12-22 23:46:19 +0500

    Update content.js

--- content/content.js
+++ content/content.js
@@ -1,9 +1,29 @@
-browser.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.action === "openAlphaXIV") {
-    // Extract ArXiv ID from the URL
-    const arXivId = window.location.pathname.split("/").pop();
-    
-    // Open AlphaXIV with the corresponding URL
+// content.js
+
+// Function to create the button
+function createAlphaXIVButton() {
+  const arXivId = window.location.pathname.split("/").pop();
+
+  // Create the button element
+  const button = document.createElement("button");
+  button.innerHTML = '<span style="margin-right: 5px;"><i class="fas fa-comment-alt"></i></span> Open Chat';
+
+  // Style the button
+  button.style.backgroundColor = "#007BFF";  // Blue color
+  button.style.color = "#FFFFFF";  // White text color
+  button.style.border = "none";
+  button.style.padding = "10px";
+  button.style.borderRadius = "5px";
+  button.style.cursor = "pointer";
+
+  // Add click event listener to open AlphaXIV
+  button.addEventListener("click", function() {
     window.open(`https://alphaxiv.org/abs/${arXivId}`, "_blank");
-  }
-});
+  });
+
+  // Append the button to the body or any desired location on the page
+  document.body.appendChild(button);
+}
+
+// Call the function to create the button
+createAlphaXIVButton();
