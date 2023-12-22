91de6e97f010e1f03f4158a796363008b35f98fd
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2023-12-22 23:21:04 +0500

    Create popup.js

---
+++ popup/popup.js
@@ -0,0 +1,7 @@
+document.getElementById("connectButton").addEventListener("click", function() {
+  // Get the current tab
+  browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    // Send a message to the content script to open AlphaXIV
+    browser.tabs.sendMessage(tabs[0].id, {action: "openAlphaXIV"});
+  });
+});
