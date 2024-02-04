9168bddc0806f3cb2d0032591218546608e50cea
rehman-talha <93119114+rehman-talha@users.noreply.github.com>
2024-02-04 23:10:02 +0500

    Update options.js

--- options.js
+++ options.js
@@ -1,3 +1,5 @@
+// options.js
+
 const saveOptions = (e) => {
   e.preventDefault();
 
@@ -26,14 +28,3 @@ const restoreOptions = async () => {
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
-
-document.getElementById('settingsForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  var notification = document.querySelector('.notification');
-  notification.style.display = 'block';
-
-  setTimeout(function () {
-    notification.style.display = 'none';
-  }, 2000);
-});
